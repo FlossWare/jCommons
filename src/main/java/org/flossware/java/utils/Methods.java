3af8cd44d527849aa7278f1216d383e5109030e3
Flossy <sfloess@redhat.com>
2023-10-08 14:46:10 -0400

    Resolves #2 - using streams instead of a traditional for-loop

--- src/main/java/org/flossware/java/utils/Methods.java
+++ src/main/java/org/flossware/java/utils/Methods.java
@@ -3,7 +3,9 @@ package org.flossware.java.utils;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Same vein as <code>java.util.Collections</code>
@@ -11,16 +13,12 @@ import java.util.List;
  * @author sfloess
  */
 public class Methods {
+    public static List<Method> findMethodsForAnnotationClass(final Class klass, final Class annotationClass) {
+        return Arrays.stream(klass.getMethods()).filter(t -> {return t.isAnnotationPresent(annotationClass);}).collect(Collectors.toCollection(ArrayList::new));
+    }
+
     public static List<Method> findMethodsForAnnotation(final Class klass, final Annotation annotation) {
-        final List<Method> retVal = new ArrayList<>(klass.getMethods().length);     // Assuming all methods have annotation.
-
-        for (final Method method : klass.getMethods()) {
-            if (null != method.getAnnotation(annotation.getClass())) {
-                retVal.add(method);
-            }
-        }
-
-        return retVal;
+        return findMethodsForAnnotationClass(klass, annotation.getClass());
     }
 
     private Methods() {
