4729288992d1397dbbf0b1b9a17c776cfea48db7
Flossy <sfloess@redhat.com>
2024-02-22 14:22:26 -0500

    NA - adding functionality to find an annotation on all methods

--- src/main/java/org/flossware/jcommons/util/MethodUtil.java
+++ src/main/java/org/flossware/jcommons/util/MethodUtil.java
@@ -31,11 +31,34 @@ public class MethodUtil {
 
     public static List<Method> findMethodsForAnnotationClass(final Class klass, final Class annotationClass) {
         // To do:  param validation
-        return Arrays.stream(klass.getMethods()).filter(t -> {return t.isAnnotationPresent(annotationClass);}).collect(Collectors.toCollection(ArrayList::new));
+        return Arrays.stream(klass.getMethods()).filter(t -> t.isAnnotationPresent(annotationClass)).collect(Collectors.toCollection(ArrayList::new));
     }
 
     public static List<Method> findMethodsForAnnotation(final Class klass, final Annotation annotation) {
         // To do:  param valdation
         return findMethodsForAnnotationClass(klass, annotation.getClass());
+    }
+
+
+    public static <T extends Annotation> T findAnnotationOnMethods(final Class klass, final Class annotationClass) {
+        // To do:  param validation
+        
+        for (final Method method : klass.getMethods()) {
+            if (null != method.getAnnotation(annotationClass)) {
+                return (T) method.getAnnotation(annotationClass);
+            }
+        }
+
+        // TO DO:  Use streams
+        //return Arrays.stream(klass.getMethods()).filter(t -> t.isAnnotationPresent(annotationClass)).collect(Collectors.toCollection(ArrayList::new));
+
+        return null;
+
+    }
+
+
+    public static <T extends Annotation> T findAnnotationOnMethods(final Class klass, final T annotation) {
+        // To do:  param validation
+        return findAnnotationOnMethods(klass, annotation.getClass());
     }
 }
